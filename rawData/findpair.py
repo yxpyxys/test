79983c69c42d958c3c6fb54a3c2fbe65b58085dd
yxpyxys <bycy456@126.com>
2022-11-15 18:15:09 -0500

    reorganize

--- rawData/findpair.py
+++ rawData/findpair.py
@@ -4,15 +4,12 @@ import pandas as pd
 from multiprocessing.pool import ThreadPool
 
 # w3 = Web3(HTTPProvider("https://damp-bitter-emerald.bsc.discover.quiknode.pro/dea7bf580630027687c7af772e1aa3b1183e72f3/"))
-w3 = Web3(IPCProvider("../node/geth.ipc"))
+w3 = Web3(IPCProvider("/root/node/geth.ipc"))
 
-with open("uni_factory.abi","r") as f:
+with open("uni_factory.abi.json","r") as f:
     factory_abi = json.loads(f.readline())
-with open("uni_pair.abi","r") as f:
+with open("uni_pair.abi.json","r") as f:
     pair_abi = json.loads(f.readline())
-
-# pancakeswap_factory = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
-# smallswap_factory = '0xD04A80baeeF12fD7b1D1ee6b1f8ad354f81bc4d7'
 
 exchange = {
     # 'pancakeswap': '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73',
@@ -35,10 +32,18 @@ exchange = {
     # 'bscswap': '0xCe8fd65646F2a2a897755A1188C04aCe94D2B8D0',
     # 'bscsswap': '0x8b6Ca4B3E08c9f80209e66436187088C99C9C2AC',
     # 'cheeseswap': '0xdd538E4Fd1b69B7863E1F741213276A6Cf1EfB3B',
-    'mdex': '0x3CD1C46068dAEa5Ebb0d3f55F6915B10648062B8',
+    # 'mdex': '0x3CD1C46068dAEa5Ebb0d3f55F6915B10648062B8',
+    # 'babyswap': '0x86407bEa2078ea5f5EB5A52B2caA963bC1F889Da',
+    # 'pancakeswapv1': '0xBCfCcbde45cE874adCB698cC183deBcF17952812',
+    # 'fastswap': '0x59DA12BDc470C8e85cA26661Ee3DCD9B85247C88',
+    # 'winery': '0x79C342FddBBF376cA6B4EFAc7aaA457D6063F8Cb',
+    # 'waultswap': '0xB42E3FE71b7E0673335b3331B3e1053BD9822570',
+    # 'cafe': '0x3e708FdbE3ADA63fc94F8F61811196f1302137AD',
+    # 'twindex': '0x4E66Fda7820c53C1a2F601F84918C375205Eac3E',
+    'alita': '0xC7a506ab3ac668EAb6bF9eCf971433D6CFeF05D9',
 }
 
-token_df = pd.read_csv("bsc_token.csv")
+token_df = pd.read_json("../data/bsc_token.json", orient='records')
 token_set = set(token_df['address'].to_list())
 print(len(token_set))
 
@@ -58,21 +63,8 @@ for e in exchange:
         t1 = p.functions.token1().call().lower()
         # if t0 in token_set and t1 in token_set:
         pair_dict[pair_address.lower()] = {"token0": t0, "token1": t1}
-        if i % 10000 == 0:
+        if i % 100 == 0:
             print(i)
-
-    # def get_one_pair(i: int):
-    #     pair_address = c.functions.allPairs(i).call()
-    #     p = w3.eth.contract(address=pair_address, abi=pair_abi)
-    #     t0 = p.functions.token0().call().lower()
-    #     t1 = p.functions.token1().call().lower()
-    #     if t0 in token_set and t1 in token_set:
-    #         pair_dict[pair_address.lower()] = {"token0": t0, "token1": t1}
-
-    # mypool = ThreadPool(5)
-    # mypool.map(get_one_pair, range(pair_len))
-    # mypool.close()
-    # mypool.join()
 
     df = pd.DataFrame.from_dict(pair_dict, orient='index')
     # df = df.reset_index()
@@ -81,4 +73,5 @@ for e in exchange:
     print(e, len(df))
     result_df = pd.concat([result_df, df])
 
-result_df.to_csv("mdxpairs.csv", index=True, index_label='address')
+# result_df.to_csv("allpair.csv", index=True, index_label='address', header=False, mode='a')
+result_df.to_csv("newpair.csv", index=True, index_label='address', header=True, mode='a')
